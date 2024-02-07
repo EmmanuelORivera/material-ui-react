8abb1ae0ddccc3e88139b1c04669f79816ae3328
Emmanuel Ochoa <eorivera95@gmail.com>
2024-02-07 15:46:55 -0600

    feat: Typography

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import { Button, Container } from '@mui/material'
+import { Button, Container, Typography } from '@mui/material'
 
 const App = () => {
   return (
     <Container>
       <h1>App</h1>
+      <Typography variant="h1">h1 with Component</Typography>
+      <Typography mt={10} variant="h1" component="h2" textAlign="center">
+        title 2
+      </Typography>
+      <Typography variant="h1" component="span">
+        span
+      </Typography>
+      <br />
       <Button variant="contained">My first button</Button>
     </Container>
   )
